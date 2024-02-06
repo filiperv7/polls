c612fbdbba949f63857d6c3836f24b4501823160
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-02-06 18:30:34 -0300

    feat: add simple poll creation route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,9 @@
 import fastify from 'fastify'
+import { createPoll } from './routes/create-poll'
 
 const app = fastify()
 
-app.get('/', (req, res) => {
-  return {
-    message: 'Hello World'
-  }
-})
+app.register(createPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server listening on http://localhost:3333 !')
