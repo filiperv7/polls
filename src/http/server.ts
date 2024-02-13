eb9d5b83b8aad84e2c44396a58967802c19a03ff
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-02-13 12:17:46 -0300

    feat: add voting logic

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
+import cookie from '@Fastify/cookie'
 import fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
 
+app.register(cookie, {
+  secret: 'app-polls',
+  hook: 'onRequest'
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server listening on http://localhost:3333 !')
